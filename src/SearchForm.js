374b5ddd9c989c163eb8f5ad3021fa89cce1cef8
alyonausoeva <alyonausus@gmail.com>
2020-09-19 15:29:56 +0300

    SearchForm

---
+++ src/SearchForm.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class  SearchForm extends Component{
+    render() {
+        return (
+            <form >
+<input type="text" placeholder="Search..."/>
+            </form>
+        )
+    }
+}
+export default SearchForm;
