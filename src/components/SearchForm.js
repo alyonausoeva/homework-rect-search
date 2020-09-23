5ce83b735102d1749631d3bdfb82f0c3970e4083
alyonausoeva <alyonausus@gmail.com>
2020-09-23 12:37:42 +0300

    prettier

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import React, {Component}  from 'react';
-
+import React, { Component } from "react";
 
 function SearchForm() {
-    const [] = useState(0);
-    return   (
-        <form >
-<input type="text" placeholder="Search here" onChange={}/>
-        </form>
-    );
-};
+  const [] = useState(0);
+  return (
+    <form>
+      <input type="text" placeholder="Search here" onChange={} />
+    </form>
+  );
+}
 
 export default SearchForm;
